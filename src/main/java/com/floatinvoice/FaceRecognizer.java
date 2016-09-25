a2473b7fc20909e4cb65aedc6f0b0c65d862a4e9
rogueplay <akhil131192@gmail.com>
2016-09-25 18:11:36 +0530

    Initial commit

--- src/main/java/com/floatinvoice/FaceRecognizer.java
+++ src/main/java/com/floatinvoice/FaceRecognizer.java
@@ -3,70 +3,93 @@ package com.floatinvoice;
 import java.io.File;
 import java.nio.IntBuffer; 
 
+import org.bytedeco.javacpp.opencv_face.LBPHFaceRecognizer;
 import org.bytedeco.javacpp.opencv_objdetect.CascadeClassifier;
-import org.bytedeco.javacpp.helper.opencv_core.CvArr;
 
 import static org.bytedeco.javacpp.opencv_face.*; 
 import static org.bytedeco.javacpp.opencv_core.*; 
 import static org.bytedeco.javacpp.opencv_imgcodecs.*; 
-import static org.bytedeco.javacpp.opencv_imgproc.*;
+
 
 public class FaceRecognizer {
-    
+
 	public static void main(String[] args) {
-        
-		String trainingDir = "E:/faces/training/";
-                
-        Mat testImage = imread("E:/faces/faces/image_0020.jpg", CV_LOAD_IMAGE_GRAYSCALE);
 
-        File root = new File(trainingDir);        
-               
-        File[] imageFiles = root.listFiles();
+		//Initializing training set
+		String trainingDir = "Test_Dataset/sample_training_set/";
 
-        MatVector images = new MatVector(imageFiles.length);
+		File trainRoot = new File(trainingDir);        
 
-        Mat labels = new Mat(imageFiles.length, 1, CV_32SC1);
-        
-        IntBuffer labelsBuf = labels.createBuffer();
+		File[] imageFiles = trainRoot.listFiles();
 
-        CascadeClassifier faceDetector=new CascadeClassifier();
-        
-        faceDetector.load("E:\\OpenCV\\opencv\\sources\\data\\haarcascades\\haarcascade_frontalface_alt.xml");
-        
-        RectVector faceDetections = new RectVector();
-        
-        int count = 0;
-        
-        for (File image : imageFiles) {
-            
-        	Mat img = imread(image.getAbsolutePath(), CV_LOAD_IMAGE_GRAYSCALE);         
-                		
-            faceDetector.detectMultiScale(img, faceDetections);           
-                     
-            Mat croppedFace=new Mat(img,faceDetections.get(0l));          
-           
-           
-          
-            
-            int label = Integer.parseInt(image.getName().split("-")[0]);
+		MatVector images = new MatVector(imageFiles.length);
 
-            images.put(count, croppedFace);
-            
-            labelsBuf.put(count, label);
+		Mat labels = new Mat(imageFiles.length, 1, CV_32SC1);
 
-            count++;  
-        }
-        
-        BasicFaceRecognizer faceRecognizer = createFisherFaceRecognizer();
+		IntBuffer labelsBuf = labels.createBuffer();
 
-        faceRecognizer.train(images, labels);
+		//Initializing test set
+		String testDir = "Test_Dataset/sample_test_set/";
 
-        faceDetector.detectMultiScale(testImage, faceDetections);           
-        
-        Mat testImageFinal = new Mat(testImage,faceDetections.get(0l)); 
-        
-        int predictedLabel = faceRecognizer.predict(testImageFinal);
+		File testRoot = new File(testDir);
 
-        System.out.println("Predicted label: " + predictedLabel);
-    }
+		File[] testFiles = testRoot.listFiles();
+
+		MatVector testImages = new MatVector(testFiles.length);
+
+		Mat testLabels = new Mat(testFiles.length, 1, CV_32SC1);
+
+		//Training face detection and recognition model  
+		CascadeClassifier faceDetector=new CascadeClassifier();
+
+		faceDetector.load("Resources/haarcascade_frontalface_alt.xml");
+
+		RectVector faceDetections = new RectVector();
+
+		int count = 0;
+
+		for (File image : imageFiles) {
+
+
+			Mat img = imread(image.getAbsolutePath(), CV_LOAD_IMAGE_GRAYSCALE);         
+
+			faceDetector.detectMultiScale(img, faceDetections);           
+
+			Mat croppedFace=new Mat(img,faceDetections.get(0l));          
+
+			int label = Integer.parseInt(image.getName().split("-")[0]);
+
+			images.put(count,croppedFace);
+
+			labelsBuf.put(count, label);
+
+			count++;  
+		}
+
+		LBPHFaceRecognizer faceRecognizer = createLBPHFaceRecognizer();
+
+		faceRecognizer.train(images, labels);
+
+		
+		//Test set Prediction
+		for (File image : testFiles) {
+
+			Mat img = imread(image.getAbsolutePath(), CV_LOAD_IMAGE_GRAYSCALE);         
+
+			faceDetector.detectMultiScale(img, faceDetections);           
+
+			Mat croppedFace=new Mat(img,faceDetections.get(0l));          
+
+			int actualLabel = Integer.parseInt(image.getName().split("-")[0]);
+			
+			System.out.println("Actual label: " + actualLabel);
+			
+			int predictedLabel = faceRecognizer.predict(croppedFace);        
+
+			System.out.println("Predicted label: " + predictedLabel);
+			
+			System.out.println("--------------------------------------------------");	
+		}
+
+	}
 }
